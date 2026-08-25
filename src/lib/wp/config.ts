1ef448e7a3457def611e2a6bb23ea1dfa5671c16
V-WebDel <viktor.webdel@gmail.com>
2026-08-25 20:35:37 +0300

    Add home WordPress diagnostics

--- src/lib/wp/config.ts
+++ src/lib/wp/config.ts
@@ -1,5 +1,9 @@
-const DEFAULT_WP_BASE_URL = "https://webdel.ru";
+export const DEFAULT_WP_BASE_URL = "https://webdel.ru";
 
 export const WP_BASE_URL = (
   process.env.NEXT_PUBLIC_WP_BASE_URL || DEFAULT_WP_BASE_URL
 ).replace(/\/$/, "");
+
+export const WP_BASE_URL_SOURCE = process.env.NEXT_PUBLIC_WP_BASE_URL
+  ? "NEXT_PUBLIC_WP_BASE_URL"
+  : "default";
